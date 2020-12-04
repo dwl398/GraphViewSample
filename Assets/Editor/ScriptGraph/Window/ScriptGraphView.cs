2cfc1570a7042bd093ebe38de367f8e7cebefab9
dwl398 <dwlzip123@gmail.com>
2020-12-04 18:44:05 +0900

    GraphView　背景追加まで

---
+++ Assets/Editor/ScriptGraph/Window/ScriptGraphView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.Experimental.GraphView;
+
+namespace ScriptGraph.Window
+{
+	public class ScriptGraphView : GraphView
+	{
+		public ScriptGraphView() : base()
+		{
+			// 親のサイズに合わせてGraphViewのサイズを設定
+			this.StretchToParentSize();
+			// ズームインアウト
+			SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+			// ドラッグで描画範囲を移動
+			this.AddManipulator(new ContentDragger());
+			// ドラッグで選択した要素を移動
+			this.AddManipulator(new SelectionDragger());
+			// ドラッグで範囲選択
+			this.AddManipulator(new RectangleSelector());
+
+			this.styleSheets.Add(Resources.Load<StyleSheet>("GraphViewBackGround"));
+
+			this.Insert(0, new GridBackground());
+		}
+	}
+}
---
+++ Assets/Editor/ScriptGraph/Window/ScriptGraphWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ScriptGraph.Window
+{
+	public class ScriptGraphWindow : EditorWindow
+	{
+		[MenuItem("Tool/ScriptGraph")]
+		public static void Open()
+		{
+			ScriptGraphWindow window = GetWindow<ScriptGraphWindow>();
+			window.Show();
+
+			window.titleContent = new GUIContent("ScriptGraph");
+		}
+
+		private void OnEnable()
+		{
+			var scriptGraph = new ScriptGraphView();
+			this.rootVisualElement.Add(scriptGraph);
+		}
+	}
+}
